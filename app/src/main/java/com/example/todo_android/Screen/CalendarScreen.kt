bbd757be242a2bccc0562b088019f990c24712bf
ParkJongJoon7128 <whdwnsdk8111@naver.com>
2023-01-19 02:14:15 +0900

    custom toggleSwitch 구현 작업중

--- app/src/main/java/com/example/todo_android/Screen/CalendarScreen.kt
+++ app/src/main/java/com/example/todo_android/Screen/CalendarScreen.kt
@@ -1,22 +1,74 @@
 package com.example.todo_android.Screen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.todo_android.Navigation.Action.RouteAction
-import com.himanshoe.kalendar.Kalendar
-import com.himanshoe.kalendar.model.KalendarType
+
 
 @Composable
 fun CalendarScreen(routeAction: RouteAction) {
+
+    val states = listOf(
+        "State 1",
+        "State 2"
+    )
+    var selectedOption by remember { mutableStateOf(states[1]) }
+
+    val onSelectionChange = { text: String -> selectedOption = text }
+
     Column(
         modifier = Modifier.fillMaxSize(),
 //        horizontalAlignment = Alignment.CenterHorizontally,
 //        verticalArrangement = Arrangement.Center
     ) {
 
-        Kalendar(kalendarType = KalendarType.Oceanic())
-        Kalendar(kalendarType = KalendarType.Firey)
+        Surface(
+            shape = RoundedCornerShape(24.dp),
+//            elevation = 4.dp,
+            modifier = Modifier
+                .wrapContentSize()
+        ) {
+            Row(
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(24.dp))
+                    .background(Color.LightGray)
+            ) {
+                states.forEach { text ->
+                    Text(
+                        text = text,
+                        color = Color.White,
+                        modifier = Modifier
+                            .clip(shape = RoundedCornerShape(24.dp))
+                            .clickable {
+                                onSelectionChange(text)
+                            }
+                            .background(
+                                if (text == selectedOption) {
+                                    Color.Magenta
+                                } else {
+                                    Color.LightGray
+                                }
+                            )
+                            .padding(
+                                vertical = 12.dp,
+                                horizontal = 16.dp,
+                            ),
+                    )
+                }
+
+//                Kalendar(kalendarType = KalendarType.Oceanic())
+//                Spacer(modifier = Modifier.height(60.dp))
+//                Kalendar(kalendarType = KalendarType.Firey)
+            }
+        }
     }
 }
