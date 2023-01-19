83a305c35307a9c29267ece614a483ce349a5015
parkjongjoon7128 <whdwnsdk8111@naver.com>
2023-01-20 00:39:55 +0900

    캘린더 테스트

--- app/src/main/java/com/example/todo_android/Screen/CalendarScreen.kt
+++ app/src/main/java/com/example/todo_android/Screen/CalendarScreen.kt
@@ -7,28 +7,31 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.todo_android.Navigation.Action.RouteAction
+import com.himanshoe.kalendar.Kalendar
+import com.himanshoe.kalendar.model.KalendarType
 
 
 @Composable
 fun CalendarScreen(routeAction: RouteAction) {
 
-    val states = listOf(
-        "State 1",
-        "State 2"
-    )
-    var selectedOption by remember { mutableStateOf(states[1]) }
-
-    val onSelectionChange = { text: String -> selectedOption = text }
+//    val states = listOf(
+//        "State 1",
+//        "State 2"
+//    )
+//    var selectedOption by remember { mutableStateOf(states[1]) }
+//
+//    val onSelectionChange = { text: String -> selectedOption = text }
 
     Column(
         modifier = Modifier.fillMaxSize(),
-//        horizontalAlignment = Alignment.CenterHorizontally,
-//        verticalArrangement = Arrangement.Center
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
 
         Surface(
@@ -37,38 +40,41 @@ fun CalendarScreen(routeAction: RouteAction) {
             modifier = Modifier
                 .wrapContentSize()
         ) {
-            Row(
-                modifier = Modifier
-                    .clip(shape = RoundedCornerShape(24.dp))
-                    .background(Color.LightGray)
-            ) {
-                states.forEach { text ->
-                    Text(
-                        text = text,
-                        color = Color.White,
-                        modifier = Modifier
-                            .clip(shape = RoundedCornerShape(24.dp))
-                            .clickable {
-                                onSelectionChange(text)
-                            }
-                            .background(
-                                if (text == selectedOption) {
-                                    Color.Magenta
-                                } else {
-                                    Color.LightGray
-                                }
-                            )
-                            .padding(
-                                vertical = 12.dp,
-                                horizontal = 16.dp,
-                            ),
-                    )
-                }
+//            Row(
+//                modifier = Modifier
+//                    .clip(shape = RoundedCornerShape(24.dp))
+//                    .background(Color.LightGray)
+//            ) {
+//                states.forEach { text ->
+//                    Text(
+//                        text = text,
+//                        color = Color.White,
+//                        modifier = Modifier
+//                            .clip(shape = RoundedCornerShape(24.dp))
+//                            .clickable {
+//                                onSelectionChange(text)
+//                            }
+//                            .background(
+//                                if (text == selectedOption) {
+//                                    Color.Magenta
+//                                } else {
+//                                    Color.LightGray
+//                                }
+//                            )
+//                            .padding(
+//                                vertical = 12.dp,
+//                                horizontal = 16.dp,
+//                            ),
+//                    )
+//                }
+//            }
+            // 가로 스크롤 커스텀 캘린더
+            Kalendar(kalendarType = KalendarType.Oceanic())
 
-//                Kalendar(kalendarType = KalendarType.Oceanic())
-//                Spacer(modifier = Modifier.height(60.dp))
-//                Kalendar(kalendarType = KalendarType.Firey)
-            }
+            Spacer(modifier = Modifier.height(20.dp))
+
+            // 기본 커스텀 캘린더
+//            Kalendar(kalendarType = KalendarType.Firey)
         }
     }
 }
