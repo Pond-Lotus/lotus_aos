7cb372bb115392bac9ea3137a5c2a688d6f9c74e
ParkJongJoon7128 <whdwnsdk8111@naver.com>
2023-02-21 23:09:19 +0900

    기본 프로필 설정 기능 구현 작업중

--- app/src/main/java/com/example/todo_android/Screen/ProfileScreen.kt
+++ app/src/main/java/com/example/todo_android/Screen/ProfileScreen.kt
@@ -134,21 +134,9 @@ fun ProfileScreen(routeAction: RouteAction) {
     decodedImage?.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
     outputStream.close()
 
-<<<<<<< HEAD
-    val defaultProfileImageBitmap =
-        BitmapFactory.decodeResource(LocalContext.current.resources, R.drawable.defaultprofile)
 
-    
-=======
     val defaultProfileImageBitmap = BitmapFactory.decodeResource(LocalContext.current.resources, R.drawable.defaultprofile)
 
-    val decodedDefaultImage = Base64.decode(defaultProfileImageBitmap, Base64.DEFAULT)
-
-    var onChangeProfileImage by remember {
-        mutableStateOf(defaultProfileImageBitmap)
-    }
-
->>>>>>> master
     val imageUri = rememberSaveable {
         mutableStateOf(decodeFile?.toUri())
     }
@@ -222,11 +210,7 @@ fun ProfileScreen(routeAction: RouteAction) {
 
                     Button(
                         onClick = {
-<<<<<<< HEAD
                             painter = onClickToDefaultImage
-=======
-                            painter = defaultProfileImageBitmap
->>>>>>> master
                             openDialog = false
                         },
                         shape = RoundedCornerShape(10.dp),
