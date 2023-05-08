821e041b6c230d5c4137bb09f22eb5d164ab8edc
ParkJongJoon7128 <whdwnsdk8111@naver.com>
2023-05-09 00:14:11 +0900

    CalendarScreen UI 작업중

--- app/src/main/java/com/example/todo_android/Screen/ProfileScreen.kt
+++ app/src/main/java/com/example/todo_android/Screen/ProfileScreen.kt
@@ -9,12 +9,10 @@ import android.util.Base64
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.focusable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -23,13 +21,19 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.KeyboardType.Companion.Uri
 import androidx.compose.ui.unit.dp
@@ -47,6 +51,16 @@ import com.example.todo_android.R
 import com.example.todo_android.Request.ModifyRequest.ChangeNicknameAndProfileRequest
 import com.example.todo_android.Response.ModifyResponse.ChangeNicknameAndProfileResponse
 import com.example.todo_android.Util.MyApplication
+import com.himanshoe.kalendar.Kalendar
+import com.himanshoe.kalendar.color.KalendarThemeColor
+import com.himanshoe.kalendar.component.day.config.KalendarDayColors
+import com.himanshoe.kalendar.component.header.config.KalendarHeaderConfig
+import com.himanshoe.kalendar.component.text.config.KalendarTextColor
+import com.himanshoe.kalendar.component.text.config.KalendarTextConfig
+import com.himanshoe.kalendar.component.text.config.KalendarTextSize
+import com.himanshoe.kalendar.model.KalendarDay
+import com.himanshoe.kalendar.model.KalendarEvent
+import com.himanshoe.kalendar.model.KalendarType
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.asRequestBody
@@ -116,7 +130,7 @@ fun changeNicknameAndProfile(
         })
 }
 
-@SuppressLint("RememberReturnType")
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @ExperimentalMaterial3Api
 @Composable
 fun ProfileScreen(routeAction: RouteAction) {
@@ -135,9 +149,10 @@ fun ProfileScreen(routeAction: RouteAction) {
     val responseDefaultProfileImage = MyApplication.prefs.getData("defaultProfileImage", "")
     val defaultProfileImageDecodedBytes = Base64.decode(responseDefaultProfileImage, Base64.DEFAULT)
     val defaultProfileImageBitmap = BitmapFactory.decodeByteArray(defaultProfileImageDecodedBytes,
-        0, defaultProfileImageDecodedBytes.size)
+        0,
+        defaultProfileImageDecodedBytes.size)
 
-    val decodeDefaultImageFile : File? = File.createTempFile("temp", null, context.cacheDir)
+    val decodeDefaultImageFile: File? = File.createTempFile("temp", null, context.cacheDir)
     val defaultOutPutStream = FileOutputStream(decodeDefaultImageFile)
     defaultProfileImageBitmap?.compress(Bitmap.CompressFormat.PNG, 100, defaultOutPutStream)
     defaultOutPutStream.close()
@@ -155,14 +170,11 @@ fun ProfileScreen(routeAction: RouteAction) {
         mutableStateOf(decodeFile?.toUri())
     }
 
-    var painter = rememberImagePainter(
-        data = imageUri.value,
-        builder = {
-            if (imageUri.value != null) {
-                placeholder(R.drawable.defaultprofile)
-            }
+    var painter = rememberImagePainter(data = imageUri.value, builder = {
+        if (imageUri.value != null) {
+            placeholder(R.drawable.defaultprofile)
         }
-    )
+    })
 
     val launcher =
         rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
@@ -188,162 +200,154 @@ fun ProfileScreen(routeAction: RouteAction) {
     }
 
 
-    if (openDialog) {
-        Dialog(
-            onDismissRequest = {
-                openDialog = false
+//    Column(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .imePadding(),
+//        horizontalAlignment = Alignment.CenterHorizontally,
+////            verticalArrangement = Arrangement.Top
+//    ) {
+//
+//        Spacer(modifier = Modifier.height(50.dp))
+//
+//        Image(
+////                painter = painter,
+//            painter = painterResource(id = R.drawable.defaultprofile),
+//            contentDescription = "profileImage",
+//            modifier = Modifier
+//                .size(150.dp)
+//                .clickable {
+//                    openDialog = !openDialog
+//                },
+//            contentScale = ContentScale.Crop
+//        )
+//    }
+
+    Scaffold(modifier = Modifier
+        .fillMaxWidth()
+        .imePadding(), topBar = {
+        CenterAlignedTopAppBar(title = {
+            Text(text = "프로필 수정",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                lineHeight = 24.sp)
+        }, navigationIcon = {
+            IconButton(onClick = {
+                routeAction.goBack()
             }) {
-            Surface(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(400.dp),
-                shape = RoundedCornerShape(12.dp),
-                color = Color.White
-            ) {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Button(
-                        onClick = {
-                            openDialog = false
-                            launcher.launch("image/*")
-                        },
-                        shape = RoundedCornerShape(10.dp),
-                        modifier = Modifier
-                            .width(250.dp)
-                            .height(45.dp)
-                            .background(Color.White))
-                    {
-                        Text(text = "앨범에서 선택")
-                    }
-
-                    Spacer(modifier = Modifier.height(15.dp))
-
-                    Button(
-                        onClick = {
-                            openDialog = false
-                            imageUri.value = decodeDefaultImageFile?.toUri()
-                        },
-                        shape = RoundedCornerShape(10.dp),
-                        modifier = Modifier
-                            .width(250.dp)
-                            .height(45.dp)
-                            .background(Color.White))
-                    {
-                        Text(text = "기본 이미지로 변경")
-                    }
-                }
+                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back")
             }
-        }
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top) {
-
-        TopAppBar(
-            title = {
-                Text(text = "프로필 수정")
-            },
-            navigationIcon = {
-                IconButton(onClick = {
-                    routeAction.goBack()
-                }) {
-                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back")
-                }
-            },
-            actions = {
-                Text(
-                    text = "완료",
-                    modifier = Modifier
-                        .padding(30.dp)
-                        .clickable {
-                            val currenNickname = MyApplication.prefs.getData("nickname", nickname)
-                            if ((body != null) || !(nickname.equals(currenNickname))) {
-                                changeNicknameAndProfile(token,
-                                    nickname,
-                                    body!!,
-                                    routeAction
-                                )
-                            }
-                        })
-            })
-
-        Spacer(modifier = Modifier.height(50.dp))
-
-        Image(
-            painter = painter,
-            contentDescription = "profileImage",
-            modifier = Modifier
-                .size(150.dp)
-                .padding(8.dp)
+        }, actions = {
+            Text(text = "완료", modifier = Modifier
+                .padding(30.dp)
                 .clickable {
-                    openDialog = !openDialog
-                },
-            contentScale = ContentScale.Crop
-        )
+                    val currenNickname = MyApplication.prefs.getData("nickname", nickname)
+                    if ((body != null) || !(nickname.equals(currenNickname))) {
+                        changeNicknameAndProfile(token, nickname, body!!, routeAction)
+                    }
+                })
+        })
+    }) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 20.dp, end = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally) {
 
-        Spacer(modifier = Modifier.height(30.dp))
+            Spacer(modifier = Modifier.padding(vertical = 36.dp))
 
-        Text(text = "닉네임")
+            Image(painter = painterResource(id = R.drawable.defaultprofile),
+                contentDescription = "profileImage",
+                modifier = Modifier
+                    .size(90.dp)
+                    .clickable {
+                        openDialog = !openDialog
+                    },
+                contentScale = ContentScale.Crop)
 
-        TextField(modifier = Modifier
-            .width(308.dp)
-            .height(54.dp),
-            value = nickname,
-            colors = TextFieldDefaults.textFieldColors(containerColor = Color(0xffF2F2F2),
-                disabledLabelColor = Color(0xffF2F2F2),
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent),
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-            shape = RoundedCornerShape(18.dp),
-            onValueChange = {
-                nickname = it
-            },
-            placeholder = {
+            Spacer(modifier = Modifier.padding(vertical = 26.dp))
+
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
                 Text(
                     text = "닉네임",
-                    fontSize = 16.sp,
-                    color = Color(0xffA9A9A9))
-            })
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp,
+                    lineHeight = 21.sp,
+                )
+            }
 
-        Spacer(modifier = Modifier.height(5.dp))
+            Spacer(modifier = Modifier.padding(vertical = 7.dp))
 
+            OutlinedTextField(modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+                value = nickname,
+                onValueChange = {
+                    nickname = it
+                },
+                colors = TextFieldDefaults.textFieldColors(containerColor = Color(0xffffffff),
+                    disabledLabelColor = Color(0xffffffff),
+                    focusedIndicatorColor = Color(0xffD0D0D0),
+                    unfocusedIndicatorColor = Color(0xffD0D0D0)))
 
-        Text(text = "이메일")
+            Spacer(modifier = Modifier.padding(vertical = 14.dp))
 
-        TextField(modifier = Modifier
-            .width(308.dp)
-            .height(54.dp)
-            .focusable(false),
-            value = MyApplication.prefs.getData("email", email),
-            colors = TextFieldDefaults.textFieldColors(containerColor = Color(0xffF2F2F2),
-                disabledLabelColor = Color(0xffF2F2F2),
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent),
-            singleLine = true,
-            readOnly = true,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-            shape = RoundedCornerShape(18.dp),
-            onValueChange = {
-                email = it
-            },
-            placeholder = {
-                Text(text = "이메일", fontSize = 16.sp, color = Color(0xffA9A9A9))
-            })
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start) {
+                Text(text = "이메일",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp,
+                    lineHeight = 21.sp)
+            }
 
-        Spacer(modifier = Modifier.height(5.dp))
+            Spacer(modifier = Modifier.padding(vertical = 7.dp))
 
-        Button(onClick = {
-            goChangePassword(NAV_ROUTE.CHANGEPASSWORD, routeAction)
-        }) {
-            Text(text = "비밀번호 변경")
+            OutlinedTextField(modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .focusable(false),
+                value = email,
+                onValueChange = {
+                    email = it
+                },
+                singleLine = true,
+                readOnly = true,
+                colors = TextFieldDefaults.textFieldColors(containerColor = Color(0xffF1F1F1),
+                    disabledLabelColor = Color(0xffF1F1F1),
+                    focusedIndicatorColor = Color(0xffD0D0D0),
+                    unfocusedIndicatorColor = Color(0xffD0D0D0)))
+
+            Spacer(modifier = Modifier.padding(vertical = 25.dp))
+
+            Button(modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .border(width = 0.5.dp,
+                    color = Color(0xff424242),
+                    shape = RoundedCornerShape(percent = 8)),
+                colors = ButtonDefaults.buttonColors(Color.White),
+                onClick = {
+                    goChangePassword(NAV_ROUTE.CHANGEPASSWORD, routeAction)
+                },
+                shape = RoundedCornerShape(8.dp)) {
+                Text(text = "비밀번호 변경",
+                    color = Color.Black,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 22.sp)
+            }
+
+            Spacer(modifier = Modifier.padding(vertical = 115.dp))
+
+            Button(modifier = Modifier
+                .width(120.dp)
+                .height(40.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xffE9E9E9)),
+                onClick = {},
+                shape = RoundedCornerShape(10.dp)) {
+                Text(text = "계정 탈퇴하기",
+                    color = Color(0xff8D8D8D),
+                    fontSize = 12.sp,
+                    lineHeight = 22.sp)
+            }
         }
     }
 }
